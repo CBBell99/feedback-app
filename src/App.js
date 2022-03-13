2826d6b29ec0257dc3678f455058975508cdd4b8
Chris Bell <chrisbeckertonbell@gmail.com>
2022-03-13 08:34:32 -0700

    create header component

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import Header from "./components/Header";
+
+function App() {
+
+  return (
+    <>
+      <Header text={true} />
+      <div className="container">
+        <h1>My App</h1>
+      </div>
+    </>
+  )
+}
+
+export default App;
