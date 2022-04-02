18c12ba74a290b2fc10b50ee12c3d1f1aa6abd14
CBBell99 <chrisbeckertonbell@gmail.com>
2022-04-02 13:40:09 -0700

    add real-time validation

--- src/components/FeedbackForm.jsx
+++ src/components/FeedbackForm.jsx
@@ -4,8 +4,21 @@ import Button from "./shared/Button"
 
 function FeedbackForm() {
   const [text, setText] = useState("");
+  const [btnDisabled, setBtnDisabled] = useState(true);
+  const [message, setMessage] = useState("");
 
   const handleTextChange = (e) => {
+    if(text === ''){
+      setBtnDisabled(true)
+      setMessage(null)
+    } else if(text !== "" && text.trim().length <= 10){
+      setMessage()
+      setBtnDisabled(true)
+    } else {
+      setMessage(null)
+      setBtnDisabled(false)
+    }
+
     setText(e.target.value);
   };
 
@@ -21,8 +34,9 @@ function FeedbackForm() {
             placeholder="Write a review"
             value={text}
           />
-          <Button type="submit" version="secondary">Send</Button>
+          <Button type="submit" isDisabled={btnDisabled}>Send</Button>
         </div>
+        {message && <div className="message">{message}</div> }
       </form>
     </Card>
   );
--- src/components/shared/Button.jsx
+++ src/components/shared/Button.jsx
@@ -1,27 +1,24 @@
-import PropTypes from "prop-types"
+import PropTypes from "prop-types";
 
 function Button({ children, version, type, isDisabled }) {
   return (
     <button type={type} disabled={isDisabled} className={`btn btn-${version}`}>
       {children}
     </button>
-
-    
-  )
+  );
 }
 
-Button.defaultProp = {
+Button.defaultProps = {
   version: "primary",
-  type: 'button',
-  isDisabled: false
-}
+  type: "button",
+  isDisabled: false,
+};
 
 Button.propTypes = {
   children: PropTypes.node.isRequired,
   version: PropTypes.string,
   type: PropTypes.string,
   isDisabled: PropTypes.bool,
+};
 
-}
-
-export default Button
+export default Button;
