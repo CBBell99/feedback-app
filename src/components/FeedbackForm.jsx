84e4bf554ffa020841992e73ebf69490d4b09ef5
CBBell99 <chrisbeckertonbell@gmail.com>
2022-04-04 05:09:26 -0700

    add feedback to app level state, install uuid

--- src/components/FeedbackForm.jsx
+++ src/components/FeedbackForm.jsx
@@ -3,7 +3,7 @@ import Card from "./shared/Card";
 import Button from "./shared/Button";
 import RatingSelect from "./RatingSelect";
 
-function FeedbackForm() {
+function FeedbackForm({ handleAdd }) {
   const [text, setText] = useState("");
   const [rating, setRating] = useState(10)
   const [btnDisabled, setBtnDisabled] = useState(true);
@@ -14,7 +14,7 @@ function FeedbackForm() {
       setBtnDisabled(true)
       setMessage(null)
     } else if(text !== "" && text.trim().length <= 10){
-      setMessage()
+      setMessage("Text must be at least 10 characters")
       setBtnDisabled(true)
     } else {
       setMessage(null)
@@ -24,9 +24,22 @@ function FeedbackForm() {
     setText(e.target.value);
   };
 
+  const handleSubmit = (e) => {
+    e.preventDefault()
+    if(text.trim().length > 10){
+      const newFeedback = {
+        text,
+        rating
+      }
+
+      handleAdd(newFeedback)
+      setText("")
+    }
+  }
+
   return (
     <Card>
-      <form>
+      <form onSubmit={handleSubmit}>
         <h2>How would you rate your service with us?</h2>
         <RatingSelect select={(rating) => setRating(rating)}/>
         <div className="input-group">
