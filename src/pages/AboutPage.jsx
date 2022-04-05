ba9818733f0ceced4167bec7b52c2e2f92b82f86
CBBell99 <chrisbeckertonbell@gmail.com>
2022-04-05 07:36:19 -0700

    create About Page

--- src/pages/AboutPage.jsx
+++ src/pages/AboutPage.jsx
@@ -1,6 +1,13 @@
+import Card from "../components/shared/Card" 
+
 function AboutPage() {
   return (
-    <div>About</div>
+    <Card>
+    <div className="about">
+      <h1>About This Project</h1>
+      <p>This is a React app to leave feedback for a product or service</p>
+    </div>
+    </Card>
   )
 }
 
