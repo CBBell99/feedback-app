43dc6355ba6e011cabc23910a9be918285461295
CBBell99 <chrisbeckertonbell@gmail.com>
2022-04-05 21:28:12 -0700

    linting errors

--- src/pages/AboutPage.jsx
+++ src/pages/AboutPage.jsx
@@ -1,14 +1,14 @@
-import Card from "../components/shared/Card" 
+import Card from "../components/shared/Card";
 
 function AboutPage() {
   return (
     <Card>
-    <div className="about">
-      <h1>About This Project</h1>
-      <p>This is a React app to leave feedback for a product or service</p>
-    </div>
+      <div className="about">
+        <h1>About This Project</h1>
+        <p>This is a React app to leave feedback for a product or service</p>
+      </div>
     </Card>
-  )
+  );
 }
 
-export default AboutPage
+export default AboutPage;
