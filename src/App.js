129f7d91e7ab7ad81dc3b0e262e0164c64aef74d
CBBell99 <chrisbeckertonbell@gmail.com>
2022-04-06 23:23:38 -0700

    create FeedbackProvider, import useContext

--- src/App.js
+++ src/App.js
@@ -8,6 +8,7 @@ import FeedbackStats from "./components/FeedbackStats";
 import FeedbackForm from "./components/FeedbackForm";
 import AboutIconLink from "./components/AboutIconLink";
 import AboutPage from "./pages/AboutPage";
+import { FeedbackProvider } from "./context/FeedbackContext"
 
 function App() {
   const [feedback, setFeedback] = useState(FeedbackData)
@@ -24,30 +25,30 @@ function App() {
   }
 
   return (
+    <FeedbackProvider>
+      <Router>
+        <Header />
 
-    <Router>
-      <Header />
+        <div className="container">
+          <Routes>
+            <Route exact path='/' element={
+              <>
+                <FeedbackForm handleAdd={addFeedback} />
+                <FeedbackStats feedback={feedback} />
+                <FeedbackList feedback={feedback} handleDelete={deleteFeedback} />
+              </>
+            }>
 
-      <div className="container">
-        <Routes>
-          <Route exact path='/' element={
-            <>
-              <FeedbackForm handleAdd={addFeedback} />
-              <FeedbackStats feedback={feedback} />
-              <FeedbackList feedback={feedback} handleDelete={deleteFeedback} />
-            </>
-          }>
+            </Route>
 
-          </Route>
+            <Route path='/about' element={<AboutPage />} />
+          </Routes>
+          <AboutIconLink />
+        </div>
 
-          <Route path='/about' element={<AboutPage />} />
-        </Routes>
-        <AboutIconLink />
-      </div>
+      </Router>
 
-    </Router>
-
-
+    </FeedbackProvider>
   )
 }
 
---
+++ src/context/FeedbackContext.jsx
@@ -0,0 +1,21 @@
+import { createContext, useState } from "react";
+
+const FeedbackContext = createContext();
+
+export const FeedbackProvider = ({children})  => {
+  const [feedback, setFeedback] = useState([
+    {
+      id:1,
+      text: "This is from context",
+      rating: 10
+    }
+  ])
+
+  return <FeedbackContext.Provider value={{
+    feedback: feedback,
+  }}>
+    {children}
+    </FeedbackContext.Provider>
+}
+
+export default FeedbackContext
