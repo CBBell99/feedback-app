ab7269d95c46d1101b92f11deeabdd20bd3ff4e1
CBBell99 <chrisbeckertonbell@gmail.com>
2022-04-07 21:19:11 -0700

    update Feedback item

--- src/context/FeedbackContext.jsx
+++ src/context/FeedbackContext.jsx
@@ -37,6 +37,12 @@ export const FeedbackProvider = ({children})  => {
     setFeedback([newFeedback, ...feedback])
   }
 
+  // Update feedback item
+  const updateFeedback = (id, updItem) => {
+    setFeedback(feedback.map((item) => item.id === id ? { ...item, ...updItem} : item))
+  }
+
+  // Set item to be updated
   const editFeedback = (item) => {
     setFeedbackEdit({
       item,
@@ -46,10 +52,11 @@ export const FeedbackProvider = ({children})  => {
 
   return <FeedbackContext.Provider value={{
     feedback: feedback,
+    feedbackEdit,
     deleteFeedback,
     addFeedback, 
     editFeedback,
-    feedbackEdit
+    updateFeedback
   }}>
     {children}
     </FeedbackContext.Provider>
