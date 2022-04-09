69862e4913b6e0823d46184ddcf4decd7f89c702
CBBell99 <chrisbeckertonbell@gmail.com>
2022-04-09 08:26:31 -0700

    update & delete requests from mock backend

--- src/context/FeedbackContext.jsx
+++ src/context/FeedbackContext.jsx
@@ -26,8 +26,10 @@ export const FeedbackProvider = ({ children }) => {
     setIsLoading(false)
   };
 
-  const deleteFeedback = (id) => {
+  const deleteFeedback = async (id) => {
     if (window.confirm("Are you sure you want to delete?")) {
+      await fetch(`/feedback/${id}`, { method: "DELETE" })
+
       setFeedback(feedback.filter((item) => item.id !== id));
     }
   };
@@ -48,9 +50,19 @@ export const FeedbackProvider = ({ children }) => {
   };
 
   // Update feedback item
-  const updateFeedback = (id, updItem) => {
+  const updateFeedback = async (id, updItem) => {
+    const response = await fetch(`/feedback/${id}`, {
+      method: "PUT",
+      headers: {
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify(updItem)
+    })
+
+    const data = await response.json()
+
     setFeedback(
-      feedback.map((item) => (item.id === id ? { ...item, ...updItem } : item))
+      feedback.map((item) => (item.id === id ? { ...item, ...data } : item))
     );
   };
 
